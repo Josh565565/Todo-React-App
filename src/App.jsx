2c7a919f0eef58f8e88c042b2f96f2fddc6b12ae
Josh565565 <smartcash565@gmail.com>
2023-06-18 08:24:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import "./App.css";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form
+            input={input}
+            setInput={setInput}
+            todods={todos}
+            setTodos={setTodos}
+          />
+        </div>
+        <div>
+          <TodoList todos={todos} setTodos={setTodos} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const TodoList = ({ todos, setTodos }) => {
+  return (
+    <div>
+      {todos.map((todo) => (
+        <li className="list-item" key={todo.id}>
+          <input
+            type="text"
+            value={todo.title}
+            className="list"
+            onChange={(event) => event.preventDefault()}
+          />
+        </li>
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
